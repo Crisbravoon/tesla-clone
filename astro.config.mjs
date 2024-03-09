3fe1535eec71fa18bf2b8beb56280b0dc39644fa
Crisbravoon <cristobal_bravo6@hotmail.com>
2024-03-08 22:17:26 -0300

    instalación de package vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,18 @@
-import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  output: 'server',
+  adapter: vercel(),
+  integrations: [tailwind()],
+  routes:[
+    { path: '/model-s', component: './src/components/ModelS.astro' },
+    { path: '/model-3', component: './src/components/Model3.astro' },
+    { path: '/model-x', component: './src/components/ModelX.astro' },
+    { path: '/model-y', component: './src/components/ModelY.astro' },
+    { path: '/powerwall', component: './src/components/Powerwall.astro' },
+    { path: '/carga', component: './src/components/Carga.astro' },
+  ]
 });
